b65511088c18d1b472c1e4da922b3ea914eb622e
siro10 <siro.white.blanc@gmail.com>
2023-01-29 02:46:05 +0900

    点数式、chromeDriverのインストール方式変更

--- LINGAbot.py
+++ LINGAbot.py
@@ -1,47 +1,54 @@
 import sys
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.chrome import service
+import chromedriver_binary
 from selenium.webdriver.common.by import By
 
 #ネットワーク読み込み待機時間
 sleepTime = 1
-#ChromeDriverのパスを設定
-CHROMEDRIVER = "!!!ここにchromedriver.exeのパスを入力!!!"
-#ChromeDriverのstartとstopを制御するServiceオブジェクトを介してパスを渡す
-chrome_service = service.Service(executable_path=CHROMEDRIVER)
+# chromeDriverオプション設定(エラーメッセージ削除)
+options = webdriver.ChromeOptions() 
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
 #Chromeを起動
-driver = webdriver.Chrome(service=chrome_service)
+driver = webdriver.Chrome(options=options)
 
 
-def main():
+def main():   
     #ログイン
     login()
     #開始、終了する問題番号の取得
-    start,end = getQuestionNumber()
+    unit,targetScore = getTargetScore()
 
-    while(start <= end):
+    while(True):
         #mode:(0)単語の意味/(1)空欄補充
         for mode in range(2):
-            selectCocet()
-            print('現在のユニット:', str(start)+'-'+str(start+24) + ("(単語の意味)" if mode ==0 else "(空欄補充)"))
-            selectUnit(mode,start)
+            moveUnit()
+            if getScore() >= targetScore:
+                return
+            print("現在のユニット:", str(unit) + "-" + str(unit+24) + ("(単語の意味)" if mode ==0 else "(空欄補充)"))
+            print("====================================")
+            selectUnit(mode,unit)
             Answer(mode)
-        start += 25
-        print("1ユニットを終了しました。")
+            print("1ユニットを終了しました。")
+        unit += 25
 
 
 def login():
     # リンガポルタを開く
-    driver.get('https://w5.linguaporta.jp/user/seibido/index.php')
+    driver.get("https://w5.linguaporta.jp/user/seibido/index.php")
     driver.set_window_size(720, 1280)
 
     for i in range(0, 3):
         try:
-            print('IDを入力')
+            """
+            print("IDを入力")
             uid = input()
-            print('パスワードを入力')
+            print("パスワードを入力")
             uPassword = input()
+            """
+
+            uid = str('i32101')
+            uPassword = ('ringo120')
 
 
             id = driver.find_element(By.XPATH, '//*[@id="content-login"]/form/table/tbody/tr[1]/td/input')
@@ -60,29 +67,34 @@ def login():
     driver.quit()
     sys.exit()
 
-def getQuestionNumber():
+def getTargetScore():
+    # 目標点数取得
     while(True):
-        print('開始する番号を入力して下さい。(1-25の場合は1, 126-150の場合は126)')
+        print("目標の点数を入力してください。(25の倍数)")
+        targetScore = int(input())
+        if targetScore % 25 == 0:
+            break
+        else:
+            print("無効な番号です。入力しなおして下さい。")
+
+    # 解答をスタートする番号の取得
+    while(True):
+        print("解答を開始する問題番号を入力してください(1-25の場合は1、226-250の場合は226)")
         start = int(input())
         if start % 25 == 1:
             break
         else:
             print("無効な番号です。入力しなおして下さい。")
+    return start,targetScore
 
-    while(True):
-            print('終了する番号を入力して下さい。(1-25の場合は25, 126-150の場合は150)')
-            end = int(input())
-            if (end % 25 == 0) and (start <= end):
-                break
-            else:
-                print("無効な番号です。入力しなおして下さい。")
-    return start,end
-
-def selectCocet():
+# ユニット選択
+def moveUnit():
     try:
         for i in range(5):
-            try:#解答終了時に開く
+            try:
+
                 driver.execute_script("document.Study.submit()") # 本の選択画面へ移行
+                
                 driver.execute_script("select_reference('70')") # cocet2600を選択
                 return
             except:
@@ -90,6 +102,13 @@ def selectCocet():
     except:
         driver.quit()
         sys.exit()
+
+# 点数の取得、終了の判定
+def getScore():
+    score = driver.find_element(By.XPATH,'//*[@id="point_table_reference"]/tbody/tr/td[1]').text
+    score = score.replace("点\n（10400点満点）","")
+    print("現在の総得点:" + score)
+    return int(score)
 
 #解答ユニット選択
 def selectUnit(mode,unit_start):
@@ -166,7 +185,7 @@ def blanc(history,question):
 def Answer(mode):
     history = {}
     while(True):
-        print("=================================================")
+        print("------------------------------------")
         
         # 問題の取得
         try:
@@ -222,7 +241,7 @@ def Answer(mode):
 
 if __name__ == '__main__':
     main()
-
-print("指定されたすべてのユニットの解答を完了しました。プログラムを終了します。")
-driver.quit()  # ブラウザーを終了する。
-sys.exit()
+    
+    print("指定された点数を超えました。プログラムを終了します。")
+    driver.quit()  # ブラウザーを終了する。
+    sys.exit()
